255eb568cad0fafdf14a99d8a8d02229a8315eb5
Anıl Gürbüz <anilgurbuz27@hotmail.com>
2019-10-08 01:33:09 +0300

    SQL Advanced Join Solutions (Oracle)

---
+++ SQL/Advanced Join/SQL Project Planning.sql
@@ -0,0 +1,9 @@
+-- https://www.hackerrank.com/challenges/sql-projects/problem
+
+select start_date, min(end_date)
+from 
+    (select start_date from projects where start_date not in (select end_date from projects)) a,
+    (select end_date from projects where end_date not in (select start_date from projects)) b 
+where start_date < end_date
+group by start_date
+order by (min(end_date) - start_date) asc, start_date asc;
